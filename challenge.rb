66d8ac0135d4df8e7673b5c20cb49e114458859b
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-10 20:53:55 +0500

    Add LinkedList addat and remove methods to the linkedList class

--- challenge.rb
+++ challenge.rb
@@ -1,4 +1,5 @@
 # Start with your code from last challenge.
+require_relative 'LinkedList.rb'
 
 list = LinkedList.new
 
@@ -6,9 +7,20 @@ list.add(3)
 list.add(5)
 list.add_at(1, 11)
 list.add_at(0, 13)
+# puts list.get(0)
+# puts list.get(1)
+# puts list.get(2)
+# puts list.get(3)
+# # => 13 3 11 5
+# puts ""
+# list.remove(0)
+# puts list.get(0)
+# puts list.get(1)
+# puts list.get(2)
+# # => 13 3 5
 
 puts list.get(2)
-# => 11
+# # => 11
 
 puts list.get(3)
 # => 5
